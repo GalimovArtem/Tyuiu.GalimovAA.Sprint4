619a78f23c95700a1751f78330fb36d12e5ebf13
Redeks Gg <ddredeks@gmail.com>
2025-09-05 00:47:48 +0500

    Task4

--- Tyuiu.GalimovAA.Sprint4.Task4.V18.Test/DataServisTest.cs
+++ Tyuiu.GalimovAA.Sprint4.Task4.V18.Test/DataServisTest.cs
@@ -1,11 +1,36 @@
-﻿namespace Tyuiu.GalimovAA.Sprint4.Task4.V18.Test
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.GalimovAA.Sprint4.Task4.V18.Lib;
+
+namespace Tyuiu.GalimovAA.Sprint4.Task4.V18.Test
 {
     [TestClass]
-    public sealed class DataServisTest
+    public class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ValidCalculate()
         {
+            DataService ds = new DataService();
+
+            int[,] matrix = new int[5, 5]
+            {
+                {4, 7, 4, 5, 4},
+                {4, 4, 5, 6, 7},
+                {7, 5, 6, 6, 6},
+                {6, 7, 6, 6, 4},
+                {5, 6, 6, 6, 5}
+            };
+
+            int[,] wait = new int[5, 5]
+            {
+                {4, 0, 4, 0, 4},
+                {4, 4, 0, 6, 0},
+                {0, 0, 6, 6, 6},
+                {6, 0, 6, 6, 4},
+                {0, 6, 6, 6, 0}
+            };
+
+            int[,] res = ds.Calculate(matrix);
+            CollectionAssert.AreEqual(wait, res);
         }
     }
 }
--- Tyuiu.GalimovAA.Sprint4.Task4.V18/Program.cs
+++ Tyuiu.GalimovAA.Sprint4.Task4.V18/Program.cs
@@ -1,2 +1,69 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Tyuiu.GalimovAA.Sprint4.Task4.V18.Lib;
+
+namespace Tyuiu.GalimovAA.Sprint4.Task4.V18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Галимов А. А. | ПКТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы. (ввод с клавиатуры)                            *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнил: Галимов Артём Азатович | ПКТб-24-1                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* значениями с клавиатуры в диапазоне от 4 до 7. Заменить нечетные        *");
+            Console.WriteLine("* элементы массива на 0.                                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[,] matrix = new int[5, 5];
+
+            Console.WriteLine("Введите элементы массива (5x5, значения от 4 до 7):");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write($"Введите элемент [{i},{j}]: ");
+                    matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("\nМассив:");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write($"{matrix[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int[,] result = ds.Calculate(matrix);
+
+            Console.WriteLine("Измененный массив (нечетные элементы заменены на 0):");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write($"{result[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
